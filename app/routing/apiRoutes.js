6f020778d0e631833abc1568cb4d99733026f65b
Devin Powell <dpowel11@kent.edu>
2019-06-15 14:39:15 -0600

    Fixed post request after I broke it

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,3 @@
-// Routing
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to 
-//     handle the compatibility logic.
-
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
@@ -15,27 +10,17 @@ var friendsData = require("../data/friends");
 // ===============================================================================
 module.exports = function (app) {
 // API GET Requests
-// Below code handles when users "visit" a page.
-// In each of the below cases when a user visits a link
-// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-// ---------------------------------------------------------------------------
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
-    });
+app.get("/api/friends", function (req, res) {
+    res.json(friendsData);
+});
 
-// API POST Requests
-// Below code handles when a user submits a form and thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate JavaScript array
-// (ex. User fills out a reservation request... this data is then sent to the server...
-// Then the server saves the data to the tableData array)
-// ---------------------------------------------------------------------------
-    app.post('/api/friends', function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-        
-    });
+// Push the JSON to the friendsArray
+app.post('/api/friends', function(req, res) {
+    friendsData.push(req.body);
+    res.json(true);
+
+    // Matchmaker
+});
 
 }
 
