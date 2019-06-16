b8ec8c7ba113502e03ebd4d870ad7f2029165003
Devin Powell <dpowel11@kent.edu>
2019-06-16 16:15:19 -0600

    Added modal popup with best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,5 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 var friendsData = require("../data/friends");
 
@@ -16,20 +14,6 @@ module.exports = function (app) {
 
     // Push the JSON to the friendsArray
     app.post('/api/friends', function (req, res) {
-        // var newFriend = req.body;
-        // res.json(newFriend);
-        // friendsData.push(newFriend);
-
-        // Matchmaker
-        // Determine the user's most compatible friend using the following as a guide:
-        // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-        // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-        // Example:
-        // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-        // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-        // Total Difference: 2 + 1 + 2 = 5
-        // Convert new user scores to integers
-
         // Best match object
         var greatMatch = {
             name: '',
@@ -37,7 +21,7 @@ module.exports = function (app) {
             matchDiff: 1000
         }
 
-
+        // Access to user information
         var userData = req.body;
         var userName = userData.name;
         var userPhoto = userData.photo;
@@ -53,7 +37,7 @@ module.exports = function (app) {
             // between the two and push that to totalDiff variable.
             for (var j = 0; j < 10; j++) {
                 totalDiff += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
-
+                // if the current matchup is lower than previous matchups, push new matchup
                 if (totalDiff <= greatMatch.matchDiff) {
                     greatMatch.name = friendsData[i].name;
                     greatMatch.photo = friendsData[i].photo;
